1b3aa8a05e8a70516d29eaab90ae7addc12431d9
Akshat Ayush <akshatayush07@gmail.com>
2025-07-23 20:20:13 -0400

    update: zellij terminal multiplexer

--- fish/functions/tmux-sessionizer.fish
+++
@@ -1,8 +0,0 @@
-function tmux-sessionizer
-  set --function name (basename (pwd) | sed -e "s/\.//g")
-  if tmux ls 2>/dev/null | grep -q "$name"
-    tmux attach -t "$name"
-  else
-    tmux new-session -s "$name"
-  end
-end
